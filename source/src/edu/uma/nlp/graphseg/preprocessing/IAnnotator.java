c9e003de43c5ac0f09e2916a8be81c7e89f43291
chkla <chkla@users.noreply.github.com>
2022-09-08 12:00:05 +0200

    Delete source/src/edu/uma/nlp/graphseg directory

--- source/src/edu/uma/nlp/graphseg/preprocessing/IAnnotator.java
+++
@@ -1,9 +0,0 @@
-package edu.uma.nlp.graphseg.preprocessing;
-
-import java.util.List;
-
-public interface IAnnotator
-{
-    void annotate(Annotation textUnit);
-    List<Annotation> annotate(String text);
-}
